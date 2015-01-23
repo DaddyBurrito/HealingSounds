94c5f0048f2635b09bbfef4d8d41e06c16417556
Daddyburrito <cgbolton1@gmail.com>
2015-01-23 17:30:54 -0600

    Initial upload of files in progress

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+
+<!-- Page created on 10/14/14 by Christopher Bolton of Bolton Web Development -->
+<!-- Copyright 2014 Healing Sounds Music Therapy                              -->
+
+<head>
+
+<meta name="keywords" content="Caregiver
+Music healing
+Music and healing
+Music therapist
+Healing and music
+Healing through music
+Healing with music
+What is guided imagery
+Guided imagery
+Guided visualization
+What is music therapy
+Benefits of music therapy
+Music as therapy
+Music for therapy
+Caregiving
+Dementia
+Alzheimer’s
+Caregiving stress
+How to deal with stress
+Caregivers for the elderly
+How to handle stress
+Caregiver depression
+Managing stress
+Spirituality
+Spiritualism
+Spiritual growth
+Spiritual development
+Spiritual health
+Spiritual journey
+Spiritual life
+Women’s health
+Women and health">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Healing Sounds Music Therapy - Home</title>
+
+<link rel="stylesheet" href="style.css" type="text/css">
+
+<!-- JavaScript required by Google Analytics to track web statistics -->
+
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-12578166-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
+
+<!-- End of Javascript for Google Analytics -->
+
+</head>
+
+    <body>
+        <div id="container"> <!-- highest level container -->
+			<div id="header">
+
+			<!-- end #header -->
+
+			</div>
+            <!-- LEFT PANEL CONTENT START -->
+            <div id="leftpanel">
+             <p><strong>Welcome to Healing Sounds Music Therapy.</strong></p> 
+                <p>Sometimes it can be difficult to seek out help for your struggles, so I appreciate you coming here.  I hope you are able to find the information you’re looking for, and I hope you will call to talk to me in person with any questions you may have.</p>
+
+                <div id="Stephanie">
+                    <img src="images/stephanie-portrait.png" alt="Stephanie Bolton" width="200" height="225" border="0" />
+                </div>  
+
+                <div>
+                    <p><em>&quot;Music has the power to truly transform our lives.&quot;</em></p>
+                    <p align="right"><span> &#45; Stephanie Bolton</span></p>
+                </div>  
+            </div>
+            <!-- LEFT PANEL CONTENT END -->
+
+			<div id="rightpanel">
+				
+			<h2>Healing Sounds Music Therapy</h2>
+				<p>We are all on a journey toward our own sense of mental, emotional, spiritual, and physical healing. Healing can include transforming old beliefs, thought patterns, and patterns of behavior into new, pro-active, healthy ones. Healing can be gaining a higher perspective that allows new perceptions of ourselves and others.</p>
+				<p>Ultimately, the journey is about finding a sense of wholeness within ourselves. We can finally affirm ourselves in our totality and find fulfillment. We release what no longer serves us and embrace those thoughts, ideas, behaviors, people, and situations that move us forward and toward an authentic self.</p>
+				<p>Many people find Guided Imagery and Music (GIM) gets to the heart of the problems they are dealing with more quickly than traditional &quot;talk&quot; therapies.  This music-centered approach works as a mind-body intervention to connect you with your own deepest wisdom as a means to develop greater self-awareness, wellness, and creativity in transforming personal, career, health, and relationship challenges.</p>
+				<p> Healing Sounds Music Therapy <strong>focuses on serving women who are experiencing life transitions</strong> such as <em>marriage</em> or <em>divorce</em>, <em>empty nest</em>, <em>motherhood</em>, <em>geographic relocation</em>, <em>job loss</em> or <em>career change</em>.  Music therapy and GIM can be for short or long-term challenges, and it can be utilized in conjunction with other traditional talk therapies to maximize results.  </p>
+
+			</div>
+
+		</div> <!-- highest level container -->
+
+    </body>
+
+</html>
+
